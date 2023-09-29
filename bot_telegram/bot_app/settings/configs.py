4e52b6f1abfc6adb4c1a2ed41e24bdef8a62b4e6
ivan.rudenko <ivan.rudenko@semiotics-lab.com>
2023-09-29 14:56:50 +0000

    Update configs.py

--- bot_telegram/bot_app/settings/configs.py
+++ bot_telegram/bot_app/settings/configs.py
@@ -1,19 +1,19 @@
-#import os
+import os
 
-web_ngrok = "https://b25d-85-114-213-169.ngrok.io"
+web_ngrok = os.environ.get("NGROK_URL")
 
-TOKEN = "6059087374:AAEg4bLEIJqF-PFkjwfIDeOSbESRrJsvvLs"
-TOKEN_DOMAIN = "72c513f9-d8f7-42ef-8e55-528bf3eef117"
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TOKEN_DOMAIN = os.environ.get("DOMAIN_TOKEN")
 
 # redis
-REDIS_HOST = "localhost"
+REDIS_HOST = "redis"
 REDIS_PORT = 6379
 REDIS_DB = 0
 REDIS_PASSWORD = ""
 
 
 # api
-BASE_HOST = "http://localhost:8888"
+BASE_HOST = "http://host.docker.internal:8000"
 url_check_number = BASE_HOST + "/api/v1/support/check_number"
 url_check_email = BASE_HOST + "/api/v1/support/check_email"
 url_news_list = BASE_HOST + "/api/v1/support/get_news_list"
@@ -21,9 +21,6 @@ url_get_news = BASE_HOST + "/api/v1/support/get_news"
 url_new_thread = BASE_HOST + "/api/v1/support/new_thread"
 url_email_code = BASE_HOST + "/api/v1/support/email_code"
 url_check_email_code = BASE_HOST + "/api/v1/support/check_email_code"
-url_send_photo = BASE_HOST + "/api/v1/support/send_photo"
-url_send_video = BASE_HOST + "/api/v1/support/send_video"
-url_send_raiting = BASE_HOST + "/api/v1/support/send_raiting"
 
 
-headers = {"Authorization": f"Bearer {TOKEN_DOMAIN}", "Accept": "application/json"}
+headers = {"DOMAIN-UUID": TOKEN_DOMAIN, "Accept": "application/json"}
